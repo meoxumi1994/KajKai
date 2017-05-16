96b9fdafe9bba46b33bae3db6d547f4f42147d43
Từ Thiện Nguyễn Văn <T Thin Nguyn Vn>
2017-05-16 21:09:10 +0700

    merge thiếu

--- client/server.js
+++ client/server.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 var express = require("express")
 var app = express()
 var fs = require('fs')
-=======
-var express = require("express");
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
->>>>>>> 2886481067b89c07c045d67fe42de5bfaea533ad
 
 app.use(express.static(__dirname + '/'))
 
@@ -15,21 +8,17 @@ app.get('*',function(req, res){
   res.sendFile(__dirname + '/index.html')
 })
 
-<<<<<<< HEAD
 var options = {
   key: fs.readFileSync('./config/kajkai.key'),
   cert: fs.readFileSync('./config/kajkai.crt')
 }
 
-require('spdy').createServer(options, app).listen(3000)
-=======
+var server = require('spdy').createServer(options, app).listen(3000)
+
+var io = require('socket.io')(server)
 io.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
     console.log(data);
   });
 });
-
-server.listen(3000)
-// app.listen(3000)
->>>>>>> 2886481067b89c07c045d67fe42de5bfaea533ad
--- server/app.js
+++ server/app.js
@@ -28,12 +28,7 @@ var corsOptions = {
   credentials: true
 }
 
-<<<<<<< HEAD
 app.use(compression())
-=======
-
-
->>>>>>> 2886481067b89c07c045d67fe42de5bfaea533ad
 app.use(cors(corsOptions));
 app.use(cookieParser())
 app.use(bodyParser.json())
