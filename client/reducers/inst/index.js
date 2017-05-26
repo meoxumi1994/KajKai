2aa77819b573099a998c64262894c1d49517b3cd
LongLH7 <lyhoanglong94@gmail.com>
2017-05-26 10:05:57 +0700

    update chat

--- client/reducers/inst/index.js
+++ client/reducers/inst/index.js
@@ -4,24 +4,18 @@ import userloginregister from './user-login-register'
 import profile from './profile'
 import registerstore from './register-store'
 import store from './store'
-<<<<<<< Updated upstream
 import target from './target'
 import entity from './entity'
-=======
 import chat from './chat'
->>>>>>> Stashed changes
 
 const inst = combineReducers({
     userloginregister,
     profile,
     registerstore,
     store,
-<<<<<<< Updated upstream
     target,
     entity,
-=======
     chat
->>>>>>> Stashed changes
 })
 
 export default inst
