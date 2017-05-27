01751143028cb423d1787878085124ae8a6d1ec1
LongLH7 <lyhoanglong94@gmail.com>
2017-05-27 14:04:01 +0700

    reverse file

--- client/config/index.js
+++ client/config/index.js
@@ -4,9 +4,9 @@ const config = {
     IP: ['localhost', '13.228.23.106'], // 13.228.23.106 10.20.16.136
     PORT: 8080,
     getDomain: () => {
-        // if(config.ISTEST)
-            // return config.PROTOCOL[1] + '://' + config.IP[1] + ':' + config.PORT;
-        // else
+        if(config.ISTEST)
+            return config.PROTOCOL[1] + '://' + config.IP[1] + ':' + config.PORT;
+        else
             return config.PROTOCOL[0] + '://' + config.IP[0] + ':' + config.PORT;
     }
 }
