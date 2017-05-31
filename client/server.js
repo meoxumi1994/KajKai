a641cb052ed294c062a52e580ccfc8ec1b4fac6d
MeoXumi <meoxumi@MeoXumis-MacBook-Pro.local>
2017-05-31 12:10:47 +0700

    ver5

--- client/server.js
+++ client/server.js
@@ -9,13 +9,13 @@ app.get('*',function(req, res){
   res.sendFile(__dirname + '/index.html')
 })
 
-var options = {
-  key: fs.readFileSync('./config/kajkai.key'),
-  cert: fs.readFileSync('./config/kajkai.crt')
-}
+// var options = {
+//   key: fs.readFileSync('./config/kajkai.key'),
+//   cert: fs.readFileSync('./config/kajkai.crt')
+// }
 
-// app.listen(3000)
-var server = require('spdy').createServer(options, app).listen(3000)
+app.listen(3000)
+// var server = require('spdy').createServer(options, app).listen(3000)
 
 // var io = require('socket.io')(server)
 // io.on('connection', function (socket) {
