9b63b5ff3a82a1eee14fbed454a8c79e12bbc192
creatnx <daihoangpham@gmail.com>
2017-06-08 00:00:48 +0700

    gettarget

---
+++ server/services/SocketService.js
@@ -0,0 +1,3 @@
+export const getUserRoomId = (id) => {
+    return 'room$' + id
+}
--- server/socketio/index.js
+++ server/socketio/index.js
@@ -5,18 +5,41 @@ import {getTokenSocketCookie} from '../utils/Utils'
 import {redisClient} from '../datasource'
 import { getSecondLayerCommentById } from '../services/CommentService'
 import { SecondLayerComment } from '../models'
+import { addUserOnline, getStatusOnline, removeUserOnline } from '../services/OnlineService'
+import { getUserRoomId } from '../services/SocketService'
 
 const init = (server) => {
     const sio = socketIo(server)
     // sio.use(cookieParser())
     sio.on('connection', (socket) => {
         console.log('a user connected')
+        var userID = null
+        if (token) {
+            var decoded = verifyToken(token)
+            if (decoded)
+                userID = decoded._id
+        }
+        if (userID) {
+            addUserOnline(userID, function (reply) {
+                if (reply) {
+                    socket.join(getUserRoomId(userID))
+                }
+            })
+        }
+
+
         socket.on('disconnect', () => {
             console.log('a user disconnected')
+            if (userID) {
+                removeUserOnline(userID, function (reply) {
+                    if (reply) {
+                        socket.leave(getUserRoomId(userID))
+                    }
+                })
+            }
         })
+
         // load all events
-
-
         for(let e in allEvents){
             let handler = allEvents[e]
             // console.log(e)
