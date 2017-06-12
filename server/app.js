b857c06b1fc095d285bfa134a4f1e961df5a1c7b
charitynguyenvan <charitynguyenvan@gmail.com>
2017-06-12 15:32:45 +0700

    edv -> deploy

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ import allRoutes from './routes'
 import { getUser } from './services/UserService'
 
 const app = express()
-var whitelist = ['https://dyrzutxpmw2we.cloudfront.net', 'http://34.209.206.70:' + config.OTHERPORT,
+var whitelist = ['https://dqp2llohmlrz8.cloudfront.net', 'http://34.209.206.70:' + config.OTHERPORT,
 'http://www.kajkai.com', 'null']
 
 
