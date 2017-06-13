a996cb5ba62fcd4a1081dd9d83975e656ab24985
charitynguyenvan <charitynguyenvan@gmail.com>
2017-06-13 12:22:27 +0700

    dev -> deploy

--- server/config/serverConfig.js
+++ server/config/serverConfig.js
@@ -1,16 +1,10 @@
 const config = {
-    ISTEST : true,
-    PROTOCOL: ['http', 'http'],
-    IP: ['localhost', '13.228.23.106'], // 34.209.206.70 10.20.16.137
+    PROTOCOL: 'https'
+    IP: 'dqp2llohmlrz8.cloudfront.net'
     PORT: 8080,
-    getDomain: () => {
-        if(config.ISTEST)
-            return config.PROTOCOL[0] + '://' + config.IP[0] + ':' + config.PORT;
-        else
-            return config.PROTOCOL[1] + '://' + config.IP[1] + ':' + config.PORT;
-    },
+    getDomain: () => (config.PROTOCOL + '://' + config.IP),
     OTHERPORT: 3000,
-    REDIRECTURL: 'http://localhost:' + config.OTHERPORT,
+    REDIRECTURL: 'https://dqp2llohmlrz8.cloudfront.net',
     FACEBOOK_API_URL: 'https://graph.facebook.com/me?fields=id,name,picture.type(large)&access_token=',
     GOOGLE_API_URL: 'https://www.googleapis.com/oauth2/v3/tokeninfo?id_token='
 }
