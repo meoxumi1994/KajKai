469886010789e81128c57ab3226e92db555d820f
charitynguyenvan <charitynguyenvan@gmail.com>
2017-06-15 18:41:14 +0700

    update api.js

--- api.js
+++ api.js
@@ -1,10 +1,4 @@
-/*format id for target
-  1. user: 001*
-  2. store: 002*
-  3. post: 003*
-*/
 GET /user {
-},{
     username: 'charity',
     imageUrl: '',
     address: undefined,
@@ -29,13 +23,13 @@ PUT /user {
     status: 'failed|success'
 }
 PUT /user/password {
-    password: password,
-    newpassword: newpassword,
+    password: '123456',
+    newpassword: '654321',
 },{
     status: 'failed|success',
 }
 PUT /user/phone {
-    phone: phone
+    phone: '0987654321'
 },{
     status: 'pending|used|error'
 }
@@ -68,7 +62,188 @@ POST /user/logingoogle {
 GET /user/logout {
 },{
 }
-GET /store/:id
+GET /store/:id {
+  id: , //store id
+  userid: ,//owner
+  storename: 'Forest',
+  avatarUrl: 'http://d2d3l62ibcj1br.cloudfront.net/hyperin-portal/imageserver/tenants/54444/_DSC3158.jpg',
+  coverUrl: 'https://tshirtstoreonline.com/client/tshirtstore/dynamic/articles/tshirt-store-guildford-view-3_4421.png',
+  address: 'fpt university',
+  category: 'shirt',
+  latitute: '1423424',
+  longitute: '453535',
+  phone: '01655791025',
+  numlike: '23',
+  likes: [
+    {
+      avatarUrl: ,// small size 20x20
+      userid: ,
+      username: ,
+    },
+  ], // tối đa 5 thằng bạn
+  numfollow: '43',
+  follows: [
+    {
+      avatarUrl: // small size 20x20
+      userid:
+      username:
+    },
+  ], // tối đa 5 thằng bạn
+}
+GET /sellpost/:storeid?offset=-1 { // length = 2
+  offset: ,
+  storeid: ,
+  sellposts: [
+    {
+      id: ,
+      storeid: ,
+      storename: ,
+      category: ,
+      title: ,
+      description: ,
+      time: , // last update
+      status: 'notyet|open|sleep',
+      ship: '', // store viết vào có thể null
+      postrows: [ // tables lấy những thằng đầu có tổng hàng <= ???, nếu quá nửa thằng cuối thì trả về cả thằng cuối
+        {
+          sellpostid:,
+          id: , //
+          content: ,
+          length: ,
+          images: [
+            // list of imageUrl
+          ], // textimage|imagetext|groupimage
+          titles: [ // for product|listproduct otherwise  null
+
+          ],
+          products: [ // for product|listproduct otherwise  null
+            {
+              id: 'product002',
+              content: ,
+              imageUrl: , // 20x20
+              list: [],
+              totalnum: // số lần được gọi trong leadercomment
+            },
+          ],
+          type: 'title|normal|product|listproduct|textimage|imagetext|groupimage',
+
+        },
+      ],
+      numlike: '23',
+      likes: [
+        {
+          userid: ,
+          username: ,
+        },
+      ], // tối đa 5 thằng bạn
+      numfollow: '43',
+      follows: [
+        {
+          userid: ,
+          username: ,
+        },
+      ], // tối đa 5 thằng bạn
+      numcomment: ,
+      numshare: ,
+      leadercomments: [ // tối đa 2 thằng trong 1 giờ
+        {
+          id: 'leadercomment001',
+          sellpostid: ,
+          order: [
+              {
+                id: 'product002',
+                content: ,
+                imageUrl: ,
+                list: [],
+                num: 2
+              },
+          ],
+          comments: [ // thằng đầu tiền là comment của leader
+            {
+              id: ,
+              ownerid: ,
+              leadercommentid: ,//
+              avatarUrl: // small size 20x20
+              name: ,
+              content: ,
+              time: ,
+              numlike: ,
+            }
+          ]
+        }
+      ]
+    },
+  ],
+}
+GET minorpost/:id?offset=-1 { // length = 3
+  offset: ,
+  minorposts: [
+    {
+      id: ,
+      storeid: ,
+      storename: ,
+      category: ,
+      title: ,
+      description: ,
+      time: , // last update
+      status: 'notyet|open|sleep',
+      ship: '', // store viết vào có thể null
+      numlike: '23',
+      likes: [
+        {
+          userid: ,
+          username: ,
+        },
+      ], // tối đa 5 thằng bạn
+      numfollow: '43',
+      follows: [
+        {
+          userid: ,
+          username: ,
+        },
+      ], // tối đa 5 thằng bạn
+      numcomment: ,
+      numshare: ,
+      leadercomments: [ // tối đa 2 thằng trong 1 giờ
+        {
+          id: 'leadercomment001',
+          sellpostid: ,
+          order: [
+              {
+                id: 'product002',
+                content: ,
+                imageUrl: ,
+                list: [],
+                num: 2
+              },
+          ],
+          comments: [ // thằng đầu tiền là comment của leader
+            {
+              id: ,
+              ownerid: ,
+              leadercommentid: ,//
+              avatarUrl: // small size 20x20
+              name: ,
+              content: ,
+              time: ,
+              numlike: ,
+            }
+          ]
+        }
+      ]
+    },
+  ],
+
+}
+GET /product/:id {
+  id: ,
+  postrowid: ,
+  imageUrl: ,
+  content: ,
+  list: [],
+  totalnum: // số lần được gọi trong leadercomment
+}
+
 POST /store/:id {
   storename: 'Táo Tàu',
   address: '47 Cầu Giáy, Hà Nội',
@@ -78,4 +253,8 @@ POST /store/:id {
   latitude: '536363',
 },{
     status: 'success|error'
+}
+
+GET /post/:id {
+
 }
