ccabdbaea82014a895daf508171c4625e6959ae3
LongLH7 <lyhoanglong94@gmail.com>
2017-06-19 21:35:21 +0700

    delete server copy

--- server copy/models/Message.js
+++
@@ -1,7 +0,0 @@
-export const Message = (userId, message, time) => {
-    return {
-        id : userId,
-        message : message,
-        time : time
-    }
-}
--- server copy/models/SubCategory.js
+++
@@ -1,7 +0,0 @@
-import mongoose from '../datasource'
-
-const SubCategorySchema = new mongoose.Schema({
-  name: { type: String, required: true },
-});
-
-module.exports = SubCategorySchema
