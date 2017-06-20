892144e4525b06dd26d19a5f8014f264e7bf52fa
creatnx <daihoangpham@gmail.com>
2017-06-20 23:06:55 +0700

    gettarget

--- command/micro-store/models/Store.js
+++ command/micro-store/models/Store.js
@@ -1,14 +1,15 @@
 import mongoose from '../datasource'
 import {AddressSchema} from './Address'
 
-export const StoreSchema = new mongoose.Schema({
+const StoreSchema = new mongoose.Schema({
     storeName: {type: String},
     address: {type: AddressSchema},
     phone: {type: String},
     category: {type: String},
     owner: {type: String},
     avatarUrl: {type: String},
+    coverUrl: {type: String},
     imageUrl: {type: String},
-    mainPostId: {type: String}
 });
 
+export default StoreSchema;
