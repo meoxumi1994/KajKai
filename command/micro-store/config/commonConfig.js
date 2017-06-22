f95017932461977620d79c779f331d3788a55cf8
charitynguyenvan <charitynguyenvan@gmail.com>
2017-06-22 14:25:44 +0700

    comfig for micro

--- command/micro-store/config/commonConfig.js
+++ command/micro-store/config/commonConfig.js
@@ -1,16 +1,13 @@
 const config = {
-    DEBUG : true,
-    PROTOCOL: 'http',
-    IP: ['localhost', '13.228.23.106'], // 34.209.206.70 10.20.16.137
-    PORT: 8080,
-    getDomain: () => {
-        if (config.DEBUG)
-            return config.PROTOCOL + '://' + config.IP[0] + ':' + config.PORT;
-        else
-            return config.PROTOCOL + '://' + config.IP[1] + ':' + config.PORT;
-    },
-    CLIENT_PORT: 8080,
-    REDIRECT_URL: 'http://www.kajkai.com',
+  SERVER_PORT: 8081,
+  CLIENT_PORT: 3000,
+  // EC2 : 13.228.23.106
+  // API Gateway : https://g9fd0yor1e.execute-api.ap-southeast-1.amazonaws.com/kajkai
+  // Cloudfront : https://dyrzutxpmw2we.cloudfront.net
+  // getServerDomain: () => ('http://localhost' + ':' + config.SERVER_PORT),
+  getServerDomain: () => ('https://g9fd0yor1e.execute-api.ap-southeast-1.amazonaws.com/kajkai'),
+  // getServerDomain: () => ('https://dyrzutxpmw2we.cloudfront.net'),
+  getClientDomain: () => ('http://localhost' + ':' + config.CLIENT_PORT)
     FACEBOOK_API_URL: 'https://graph.facebook.com/me?fields=id,name,picture.type(large)&access_token=',
     GOOGLE_API_URL: 'https://www.googleapis.com/oauth2/v3/tokeninfo?id_token='
 }
