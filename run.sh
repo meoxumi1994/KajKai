7403dd2b2947dfd6b5127422de6391364de0ba93
charitynguyenvan <charitynguyenvan@gmail.com>
2017-06-28 23:20:52 +0700

    up code

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 pm2 kill &&
 
-pm2 start ./event-store/index-event.js &&
+pm2 start ./event-store/indes-event.js &&
 
 pm2 start ./command/micro-user/index-user.js &&
 pm2 start ./command/micro-store/index-store.js &&
