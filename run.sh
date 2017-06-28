87d90ab21c474238b91052a3334dd74cd143e3f8
charitynguyenvan <charitynguyenvan@gmail.com>
2017-06-29 00:25:13 +0700

    run2.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,7 @@
 pm2 kill &&
 
-pm2 start ./event-store/indes-event.js &&
-
 pm2 start ./command/micro-user/index-user.js &&
 pm2 start ./command/micro-store/index-store.js &&
 pm2 start ./command/micro-socket/index-socket.js &&
 pm2 start ./command/micro-comment/index-comment.js &&
-pm2 start ./command/micro-chat/index-chat.js &&
-
-pm2 start ./query/view-user/indev-user.js &&
-pm2 start ./query/view-store/indev-store.js &&
-pm2 start ./query/view-post/indev-post.js &&
-pm2 start ./query/view-chat/indev-chat.js
+pm2 start ./command/micro-chat/index-chat.js
