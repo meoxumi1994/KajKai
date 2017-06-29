7855ab076571ce2c028b9561f7b59165d1004e39
creatnx <daihoangpham@gmail.com>
2017-06-29 15:31:59 +0700

    fuck rage quit

--- command/micro-user/app.js
+++ command/micro-user/app.js
@@ -6,28 +6,33 @@ import bodyParser from 'body-parser'
 import config from './config/commonConfig'
 import allRoutes from './routes'
 
-const app = express()
+const app = express();
 
 const corsOptions = {
     origin: config.getClientDomain(),
     credentials: true
 };
 
-app.use(compression())
-app.use(cors(corsOptions))
-app.use(cookieParser())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+console.log(corsOptions);
+console.log(config);
+console.log(config.getClientDomain());
+
+app.use(compression());
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // load all routes
 for(let link in allRoutes){
-  let allMethods = allRoutes[link]
-  for(let requestMethod in allMethods){
-    let handler = allMethods[requestMethod]
-    let method = require('./controllers/' + handler.controller)[handler.method]
+    let allMethods = allRoutes[link];
+    for(let requestMethod in allMethods){
+        let handler = allMethods[requestMethod];
+        let method = require('./controllers/' + handler.controller)[handler.method];
+        console.log(method);
 
-    app[requestMethod](link, handler.middleware || [], method())
-  }
+        app[requestMethod](link, handler.middleware || [], method())
+    }
 }
 
 export default app
