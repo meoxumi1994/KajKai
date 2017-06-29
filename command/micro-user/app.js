dfc4bd91af7ce3fdc6d1742bdcf31c72bdbaed81
charitynguyenvan <charitynguyenvan@gmail.com>
2017-06-29 14:37:01 +0700

    .

--- command/micro-user/app.js
+++ command/micro-user/app.js
@@ -10,7 +10,7 @@ const app = express()
 
 const corsOptions = {
     origin: config.getClientDomain(),
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: true,
     credentials: true
 };
 
