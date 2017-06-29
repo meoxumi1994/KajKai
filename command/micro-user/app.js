61cfb106b9e1d9780ed4faa4e23d79cb2b3254ac
creatnx <daihoangpham@gmail.com>
2017-06-29 15:40:22 +0700

    fuck rage quit

--- command/micro-user/app.js
+++ command/micro-user/app.js
@@ -8,33 +8,34 @@ import allRoutes from './routes'
 
 const app = express()
 
-// const corsOptions = {
-//     origin: config.getClientDomain(),
-//     credentials: true
-// };
+const corsOptions = {
+    origin: 'fuck:3000',//config.getClientDomain(),
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+};
 //
 // app.options('*', cors(corsOptions))
 app.use(compression())
-// app.use(cors(corsOptions))
+app.use(cors(corsOptions))
 
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+// app.use(function (req, res, next) {
+//
+//     // Website you wish to allow to connect
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//
+//     // Pass to next layer of middleware
+//     next();
+// });
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
