05dc88e600bc47728b31d616ef992ce1a359a8e6
creatnx <daihoangpham@gmail.com>
2017-07-02 01:07:46 +0700

    fuck rage quit

--- command/micro-socket/utils/utils.js
+++ command/micro-socket/utils/utils.js
@@ -1,4 +1,4 @@
-import { uuidv4 } from 'uuid/v4'
+// import { uuidv4 } from 'uuid/v4'
 
 const phoneRegrex = /^\+?\d{1,3}?[- .]?\(?(?:\d{2,3})\)?[- .]?\d\d\d[- .]?\d\d\d\d$/
 const emailRegrex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
@@ -45,6 +45,6 @@ export const getCurrentTime = () => {
     return (new Date()).getTime()
 };
 
-export const getUUID = () => {
-    return uuidv4();
-};
+// export const getUUID = () => {
+//     return uuidv4();
+// };
