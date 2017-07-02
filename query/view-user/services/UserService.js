f254b3c2259b9c96f07c4db32359478d63fd2719
charitynguyenvan <charitynguyenvan@gmail.com>
2017-07-02 20:48:47 +0700

    status for get User

--- query/view-user/services/UserService.js
+++ query/view-user/services/UserService.js
@@ -8,7 +8,10 @@ export const getUser = (id, next) => {
           next(null)
         } else {
           next({
-            id
+            status: 'nodata',
+            user: {
+              id
+            }
           })
         }
       } else {
