0ad1c8e23a4e619ac955136cd40c539afb799c79
charitynguyenvan <charitynguyenvan@gmail.com>
2017-07-03 15:58:02 +0700

    .

--- command/micro-chat/datasource.js
+++ command/micro-chat/datasource.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose'
 import redis from 'redis'
+import config from './config/pubSubConfig'
 
-export const redisClient = redis.createClient()
+export const redisClient = redis.createClient(config)
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/kajkai-chat', { useMongoClient: true }, (err) => {
     if (err) {
--- command/micro-comment/datasource.js
+++ command/micro-comment/datasource.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose'
 import redis from 'redis'
+import config from './config/pubSubConfig'
 
-export const redisClient = redis.createClient()
+export const redisClient = redis.createClient(config)
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/kajkai-comment', { useMongoClient: true }, (err) => {
     if (err) {
