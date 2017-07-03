c2a6a2890c6067b4626244a6d9f4e94ebe597058
creatnx <daihoangpham@gmail.com>
2017-07-04 02:53:13 +0700

    fuck rage quit

--- command/micro-user/datasource.js
+++ command/micro-user/datasource.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose'
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/kajkai-user', { useMongoClient: true }, (err) => {
+mongoose.connect('mongodb://localhost/kajkai-user', { useMongoClient: true, user: 'bohuc', pass: 'mimodern'}, (err) => {
     if (err) {
         console.log('error login mongoose', err)
     }
-})
+});
 export default mongoose
