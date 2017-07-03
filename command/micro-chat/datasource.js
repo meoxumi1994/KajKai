1047014fba5b77ed296f583c607268a8219ddcdd
charitynguyenvan <charitynguyenvan@gmail.com>
2017-07-03 20:35:51 +0700

    up code

--- command/micro-chat/datasource.js
+++ command/micro-chat/datasource.js
@@ -1,8 +1,6 @@
 import mongoose from 'mongoose'
-import redis from 'redis'
 import config from './config/pubSubConfig'
 
-export const redisClient = redis.createClient(config)
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/kajkai-chat', { useMongoClient: true }, (err) => {
     if (err) {
--- command/micro-comment/datasource.js
+++ command/micro-comment/datasource.js
@@ -1,8 +1,6 @@
 import mongoose from 'mongoose'
-import redis from 'redis'
 import config from './config/pubSubConfig'
 
-export const redisClient = redis.createClient(config)
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/kajkai-comment', { useMongoClient: true }, (err) => {
     if (err) {
