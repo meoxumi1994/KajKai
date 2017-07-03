d1c227a54634d4e45a0b44235b234d0503faeacc
creatnx <daihoangpham@gmail.com>
2017-07-04 02:51:24 +0700

    fuck rage quit

--- command/micro-socket/datasource.js
+++ command/micro-socket/datasource.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/kajkai-socket', { useMongoClient: true }, (err) => {
+mongoose.connect('mongodb://localhost/kajkai-socket', { useMongoClient: true, user: 'bohuc', pass: 'mimodern' }, (err) => {
     if (err) {
         console.log('error login mongoose', err)
     }
