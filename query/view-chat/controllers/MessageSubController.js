56059c0fb35a17288379cce4a3f80e7ad250ae3f
charitynguyenvan <charitynguyenvan@gmail.com>
2017-07-04 17:31:22 +0700

    .

--- query/view-chat/controllers/MessageSubController.js
+++ query/view-chat/controllers/MessageSubController.js
@@ -3,15 +3,15 @@ import { UserChat, Chat, Message, Content } from '../models'
 export const createMessage = (message) => {
   const { mesId: id, senderId: userId, message: content, time } = message.message
 
-  const mMessage = {
+  const mMessage = new Message({
     userId
-  }
+  })
 
-  const mContent = {
+  const mContent = new Content({
     text: content.text,
     type: content.type,
     url: content.url
-  }
+  })
 
   if (content) mMessage.content = mContent
   if (time) mMessage.time = time
@@ -25,7 +25,9 @@ export const createMessage = (message) => {
       messages = [...messages, mMessage]
       chat.lastMessageTime = mMessage.time
       chat.messages = messages
-      console.log('messages', JSON.stringify(messages));
+      var test = messages;
+      console.log('test',  JSON.stringify(test));
+      console.log('messages Long', JSON.stringify(messages));
       console.log('chat.messages', JSON.stringify(chat.messages));
       console.log('chat', JSON.stringify(chat));
       chat.save((err) => {
