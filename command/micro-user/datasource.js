5b11825b61dffa053cef07242df44ff7e91885c9
charitynguyenvan <charitynguyenvan@gmail.com>
2017-07-09 12:19:21 +0700

    .

--- command/micro-user/datasource.js
+++ command/micro-user/datasource.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://admin:admin@localhost/kajkai', {
+mongoose.connect('mongodb://admin:admin@localhost/kajkai-user', {
   useMongoClient: true
 }, (err) => {
     if (err) {
