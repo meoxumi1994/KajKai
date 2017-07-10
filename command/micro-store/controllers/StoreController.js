d93cf3db7f8f0538165446365394f71d64a6bc84
creatnx <daihoangpham@gmail.com>
2017-07-10 22:09:02 +0700

    fuck rage quit

--- command/micro-store/controllers/StoreController.js
+++ command/micro-store/controllers/StoreController.js
@@ -2,6 +2,7 @@ import { createStore, updateStore, getStoreBasicInfoService } from '../services/
 
 export const addStoreCon = () => {
     return (req, res) => {
+        console.log(JSON.stringify(req.body));
         createStore(req.body, (store) => {
             res.json({...req.body, storeId: getStoreBasicInfoService(store).storeId})
         })
