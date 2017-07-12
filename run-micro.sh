9543497a762819d9ed33f1d313fa03161fdab65f
charitynguyenvan <charitynguyenvan@gmail.com>
2017-07-12 17:01:30 +0700

    .

--- run-micro.sh
+++ run-micro.sh
@@ -1,9 +1,9 @@
 pm2 kill &&
 
-mongo -u admin -p admin --authenticationDatabase admin kajkai-user --eval "db.dropDatabase()" &&
-mongo -u admin -p admin --authenticationDatabase admin kajkai-store --eval "db.dropDatabase() && db.getCollection('categories').createIndex( { name: \"text\" } )" &&
-mongo -u admin -p admin --authenticationDatabase admin kajkai-comment --eval "db.dropDatabase()" &&
-mongo -u admin -p admin --authenticationDatabase admin kajkai-chat --eval "db.dropDatabase()" &&
+mongo -u admin -p dbjfu9cyr82bt2cpqbtuiavcp49q7vp48cq09xpnbu --authenticationDatabase admin kajkai-user --eval "db.dropDatabase()" &&
+mongo -u admin -p dbjfu9cyr82bt2cpqbtuiavcp49q7vp48cq09xpnbu --authenticationDatabase admin kajkai-store --eval "db.dropDatabase() && db.getCollection('categories').createIndex( { name: \"text\" } )" &&
+mongo -u admin -p dbjfu9cyr82bt2cpqbtuiavcp49q7vp48cq09xpnbu --authenticationDatabase admin kajkai-comment --eval "db.dropDatabase()" &&
+mongo -u admin -p dbjfu9cyr82bt2cpqbtuiavcp49q7vp48cq09xpnbu --authenticationDatabase admin kajkai-chat --eval "db.dropDatabase()" &&
 
 pm2 start ./command/micro-user/index-user.js &&
 pm2 start ./command/micro-store/index-store.js &&
