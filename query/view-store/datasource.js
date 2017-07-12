3be729e4565cbadd7bec10a9223c6d2e510614d1
charitynguyenvan <charitynguyenvan@gmail.com>
2017-07-12 17:08:50 +0700

    up code

--- query/view-store/datasource.js
+++ query/view-store/datasource.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://admin:admin@localhost/kajkav-store', {
+mongoose.connect('mongodb://admin:dbjfu9cyr82bt2cpqbtuiavcp49q7vp48cq09xpnbu@localhost/kajkav-store', {
   useMongoClient: true
 }, (err) => {
     if (err) {
--- query/view-user/datasource.js
+++ query/view-user/datasource.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://admin:admin@localhost/kajkav-user', {
+mongoose.connect('mongodb://admin:dbjfu9cyr82bt2cpqbtuiavcp49q7vp48cq09xpnbu@localhost/kajkav-user', {
   useMongoClient: true
 }, (err) => {
     if (err) {
