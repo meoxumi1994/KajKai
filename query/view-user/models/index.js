0c11bbc934a4e3c409e9a28b3e0c8a9f8e6d1736
charitynguyenvan <charitynguyenvan@gmail.com>
2017-07-13 18:17:55 +0700

    up code

--- query/view-user/models/index.js
+++ query/view-user/models/index.js
@@ -10,7 +10,7 @@ import UserSchema from './User'
 export const Address = mongoose.model('Address', AddressSchema)
 export const BasicStore = mongoose.model('BasicStore', BasicStoreSchema)
 export const Black = mongoose.model('Black', BlackSchema)
-export const ImageSchema = mongoose.model('Image', ImageSchema)
+export const Image = mongoose.model('Image', ImageSchema)
 export const LastUpdate = mongoose.model('LastUpdate', LastUpdateSchema)
 export const Privacy = mongoose.model('Privacy', PrivacySchema)
 export const User = mongoose.model('User', UserSchema)
