0d090adc5dc4d8980872e54c6576774b3a4587e9
charitynguyenvan <charitynguyenvan@gmail.com>
2017-07-14 23:34:16 +0700

    up code

--- query/view-store/utils/index.js
+++ query/view-store/utils/index.js
@@ -1,3 +1,6 @@
 export const checkInside = (latitude, longitude, swlat, swlng, nelat, nelng) => {
-  return swlat <= latitude && latitude <= nelat && swlng <= longitude && longitude <= nelng
+  return swlat <= latitude && latitude <= nelat &&
+  ((swlng <= nelng && swlng <= longitude && longitude <= nelng ) ||
+   (swlng > nelng && ((swlng <= longitude && longitude <= 180) ||
+                      (-180 <= longitude && longitude <= nelng))))
 }
