1b560c47e2abe8253aa0b4f7ea9102a5a9c32f8f
creatnx <daihoangpham@gmail.com>
2017-07-17 10:03:20 +0700

    fuck rage quit

--- command/micro-store/controllers/StoreSubController.js
+++ command/micro-store/controllers/StoreSubController.js
@@ -15,6 +15,7 @@ export const getStoreSub = (message, next) => {
 export const getStoreFromPostSub = (message, next) => {
     const id = message.postId;
     console.log('fuck this shit ' + JSON.stringify(message));
+    console.log('shit fuck ' + id.toString(), id.toString().startsWith('012'));
     if (id.toString().startsWith(config.SELLPOST_GLOBAL_ID)) {
         console.log('this ' + JSON.stringify(id));
         getStoreFromSellPostId(id, (store) => {
@@ -26,6 +27,14 @@ export const getStoreFromPostSub = (message, next) => {
             }
         })
     }
+    getStoreFromSellPostId(id, (store) => {
+        console.log('this ' + JSON.stringify(store));
+        if (store) {
+            next({status: 'success', store: getStoreInfoService(store)});
+        } else {
+            next({status: 'failed'});
+        }
+    })
 };
 
 export const getStoreListSub = (message, next) => {
