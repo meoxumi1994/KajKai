565a40216b85bf39c8f54e6ea338606eaf6cf97f
charitynguyenvan <charitynguyenvan@gmail.com>
2017-07-17 13:00:17 +0700

    up code

--- query/view-post/controllers/CommentSubController.js
+++ query/view-post/controllers/CommentSubController.js
@@ -1,4 +1,4 @@
-import { Sellpost, Minorpost, Comment, Reply, BasicUser } from '../models'
+import { Sellpost, Minorpost, Comment, Reply, BasicUser, BasicStore } from '../models'
 
 export const createComment = (message) => {
   const { fCommentId: id, posterId: userId, sellPostId: sellpostId, minorPostId: minorpostId, order, time, content } = message.fComment
@@ -17,78 +17,159 @@ export const createComment = (message) => {
     numberOfOrder: product.num
   }))
 
-  BasicUser.findOne({ id: userId }, (err, basicUser) => {
-    if (basicUser) {
-      const reply = new Reply({
-        commentId: id,
-        id,
-        userId,
-        username: basicUser.username,
-        avatarUrl: basicUser.avatarUrl,
-        content,
-        time,
-        numberOfLike: 0
-      })
+  if(userId.substr(0, 3) == '001') {
+    BasicUser.findOne({ id: userId }, (err, basicUser) => {
+      if (basicUser) {
+        const reply = new Reply({
+          commentId: id,
+          type: 'user',
+          id,
+          userId,
+          username: basicUser.username,
+          avatarUrl: basicUser.avatarUrl,
+          content,
+          time,
+          numberOfLike: 0
+        })
 
-      comment.replies = []
-      comment.replies.push(reply)
-      comment.save(() => {})
+        comment.replies = []
+        comment.replies.push(reply)
+        comment.save(() => {})
 
-      if (sellpostId) {
-        Sellpost.findOne({ id: sellpostId }, (err, sellpost) => {
-          if (sellpost) {
-            let { comments } = sellpost
+        if (sellpostId) {
+          Sellpost.findOne({ id: sellpostId }, (err, sellpost) => {
+            if (sellpost) {
+              let { comments } = sellpost
 
-            if (!comments) {
-              comments = []
-            }
-            comments.push(comment)
-            sellpost.numberOfComment = comments.length
-            sellpost.comments = comments
-
-            if (order) {
-              const { postrows } = sellpost
-              for (let i = 0; i < postrows.length; i++) {
-                let { products } = postrows[i]
-                if (products) {
-                  for (let k = 0; k < products.length; k++) {
-                    let product = products[k]
-
-                    order.map((orderedProduct) => {
-                      if (orderedProduct.id == product.id) {
-                        if (product.numberOfOrder) {
-                          product.numberOfOrder += orderedProduct.numberOfOrder
-                        } else {
-                          product.numberOfOrder = orderedProduct.numberOfOrder
-                        }
-                      }
-                    })
-                    products[k] = product
-                  }
-                  postrows[i].products = products
-                }
+              if (!comments) {
+                comments = []
               }
+              comments.push(comment)
+              sellpost.numberOfComment = comments.length
+              sellpost.comments = comments
 
-              sellpost.postrows = postrows
-            }
-            sellpost.save(() => {})
-          }
-        })
-      } else {
-        Minorpost.findOne({ id: minorpostId }, (err, minorpost) => {
-          if (minorpost) {
-            let { comments } = minorpost
+              if (order) {
+                const { postrows } = sellpost
+                for (let i = 0; i < postrows.length; i++) {
+                  let { products } = postrows[i]
+                  if (products) {
+                    for (let k = 0; k < products.length; k++) {
+                      let product = products[k]
 
-            if (!comments) {
-              comments = []
+                      order.map((orderedProduct) => {
+                        if (orderedProduct.id == product.id) {
+                          if (product.numberOfOrder) {
+                            product.numberOfOrder += orderedProduct.numberOfOrder
+                          } else {
+                            product.numberOfOrder = orderedProduct.numberOfOrder
+                          }
+                        }
+                      })
+                      products[k] = product
+                    }
+                    postrows[i].products = products
+                  }
+                }
+
+                sellpost.postrows = postrows
+              }
+              sellpost.save(() => {})
             }
-            comments.push(comment)
-            minorpost.numberOfComment = comments.length
-            minorpost.comments = comments
-            minorpost.save(() => {})
-          }
-        })
+          })
+        } else {
+          Minorpost.findOne({ id: minorpostId }, (err, minorpost) => {
+            if (minorpost) {
+              let { comments } = minorpost
+
+              if (!comments) {
+                comments = []
+              }
+              comments.push(comment)
+              minorpost.numberOfComment = comments.length
+              minorpost.comments = comments
+              minorpost.save(() => {})
+            }
+          })
+        }
       }
-    }
-  })
+    })
+  } else {
+    BasicStore.findOne({ id: userId }, (err, basicStore) => {
+      if (basicStore) {
+        const reply = new Reply({
+          commentId: id,
+          type: 'store',
+          id,
+          userId,
+          username: basicStore.storeName,
+          avatarUrl: basicStore.avatarUrl,
+          content,
+          time,
+          numberOfLike: 0
+        })
+
+        comment.replies = []
+        comment.replies.push(reply)
+        comment.save(() => {})
+
+        if (sellpostId) {
+          Sellpost.findOne({ id: sellpostId }, (err, sellpost) => {
+            if (sellpost) {
+              let { comments } = sellpost
+
+              if (!comments) {
+                comments = []
+              }
+              comments.push(comment)
+              sellpost.numberOfComment = comments.length
+              sellpost.comments = comments
+
+              if (order) {
+                const { postrows } = sellpost
+                for (let i = 0; i < postrows.length; i++) {
+                  let { products } = postrows[i]
+                  if (products) {
+                    for (let k = 0; k < products.length; k++) {
+                      let product = products[k]
+
+                      order.map((orderedProduct) => {
+                        if (orderedProduct.id == product.id) {
+                          if (product.numberOfOrder) {
+                            product.numberOfOrder += orderedProduct.numberOfOrder
+                          } else {
+                            product.numberOfOrder = orderedProduct.numberOfOrder
+                          }
+                        }
+                      })
+                      products[k] = product
+                    }
+                    postrows[i].products = products
+                  }
+                }
+
+                sellpost.postrows = postrows
+              }
+              sellpost.save(() => {})
+            }
+          })
+        } else {
+          Minorpost.findOne({ id: minorpostId }, (err, minorpost) => {
+            if (minorpost) {
+              let { comments } = minorpost
+
+              if (!comments) {
+                comments = []
+              }
+              comments.push(comment)
+              minorpost.numberOfComment = comments.length
+              minorpost.comments = comments
+              minorpost.save(() => {})
+            }
+          })
+        }
+      }
+    })
+  }
+
+
 }
--- query/view-post/models/Reply.js
+++ query/view-post/models/Reply.js
@@ -2,6 +2,7 @@ import mongoose from '../datasource'
 
 const ReplySchema = new mongoose.Schema({
   commentId: {type: String},
+  type: {type: String},
   id: {type: String},
   userId: {type: String},
   username: {type: String},
