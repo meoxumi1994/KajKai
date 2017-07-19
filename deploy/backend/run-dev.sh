fc74dfe43fcadc0e65436c927db511cadfc7b10b
charitynguyenvan <charitynguyenvan@gmail.com>
2017-07-20 01:55:47 +0700

    up code

---
+++ deploy/backend/run-dev.sh
@@ -0,0 +1,10 @@
+cp ./dev/micro-chat.js ../../command/micro-chat/config/commonConfig.js &&
+cp ./dev/micro-comment.js ../../command/micro-comment/config/commonConfig.js &&
+cp ./dev/micro-notification.js ../../command/micro-notification/config/commonConfig.js &&
+cp ./dev/micro-search.js ../../command/micro-search/config/commonConfig.js &&
+cp ./dev/micro-store.js ../../command/micro-store/config/commonConfig.js &&
+cp ./dev/micro-user.js ../../command/micro-user/config/commonConfig.js &&
+cp ./dev/view-chat.js ../../query/view-chat/config/commonConfig.js &&
+cp ./dev/view-post.js ../../query/view-post/config/commonConfig.js &&
+cp ./dev/view-store.js ../../query/view-store/config/commonConfig.js &&
+cp ./dev/view-user.js ../../query/view-user/config/commonConfig.js
---
+++ deploy/backend/run-prod.sh
@@ -0,0 +1,10 @@
+cp ./prod/micro-chat.js ../../command/micro-chat/config/commonConfig.js &&
+cp ./prod/micro-comment.js ../../command/micro-comment/config/commonConfig.js &&
+cp ./prod/micro-notification.js ../../command/micro-notification/config/commonConfig.js &&
+cp ./prod/micro-search.js ../../command/micro-search/config/commonConfig.js &&
+cp ./prod/micro-store.js ../../command/micro-store/config/commonConfig.js &&
+cp ./prod/micro-user.js ../../command/micro-user/config/commonConfig.js &&
+cp ./prod/view-chat.js ../../query/view-chat/config/commonConfig.js &&
+cp ./prod/view-post.js ../../query/view-post/config/commonConfig.js &&
+cp ./prod/view-store.js ../../query/view-store/config/commonConfig.js &&
+cp ./prod/view-user.js ../../query/view-user/config/commonConfig.js
