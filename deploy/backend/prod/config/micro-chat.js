aca477d24119603c5e5e909afbee5f8c9fec4a49
charitynguyenvan <charitynguyenvan@gmail.com>
2017-07-21 03:41:06 +0700

    up code

--- deploy/backend/prod/config/micro-chat.js
+++ deploy/backend/prod/config/micro-chat.js
@@ -1,8 +1,8 @@
 const config = {
     SERVER_PORT: 8083,
     getServerDomain: () => ('https://sl2zt6it2h.execute-api.ap-southeast-1.amazonaws.com/kajkai_prod'),
-    getClientDomain: () => ('https://kajkai.com'),
+    getClientDomain: () => ('https://www.kajkai.com'),
     getDataSource: () => ('mongodb://admin:dbjfu9cyr82bt2cpqbtuiavcp49q7vp48cq09xpnbu@54.254.219.177/kajkai-chat')
-};
+}
 
 export default config
