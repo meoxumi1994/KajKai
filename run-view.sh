434bdd8e621b12c97b3176dffb597223ed446bef
charitynguyenvan <charitynguyenvan@gmail.com>
2017-07-23 14:28:41 +0700

    up code

--- run-view.sh
+++ run-view.sh
@@ -13,4 +13,5 @@ pm2 start ./event-store/indes-event.js &&
 pm2 start ./query/view-user/indev-user.js &&
 pm2 start ./query/view-store/indev-store.js &&
 pm2 start ./query/view-post/indev-post.js &&
+
 pm2 start ./query/view-chat/indev-chat.js
