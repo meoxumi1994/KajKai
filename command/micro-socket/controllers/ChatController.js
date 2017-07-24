d637acf9754d4650b35be8a5ad048879d131a1fc
creatnx <daihoangpham@gmail.com>
2017-07-24 12:39:04 +0700

    ~~

--- command/micro-socket/controllers/ChatController.js
+++ command/micro-socket/controllers/ChatController.js
@@ -13,12 +13,14 @@ export const addNewMessageCon = (action, sio, io) => {
 };
 
 export const getUnreadMessageCon = (userID, sio, io) => {
+    if (!action.data.userID) return;
     getUnreadMessagePub(userID, (data) => {
         sio.emit('action', {type: 'global/UNREAD_CHATS', data: data})
     })
 };
 
 export const resetChatCountCon = (action, sio, io) => {
+    if (!action.data.userID) return;
     resetChatCountPub(action.data.userID, (res) => {
         sio.emit('action', {type: 'client/RESET_UNREAD_CHATS_QUANTITY', data: {
             quantity: 0,
@@ -27,12 +29,14 @@ export const resetChatCountCon = (action, sio, io) => {
 };
 
 export const readChatCon = (action, sio, io) => {
+    if (!action.data.userID) return;
     readChatPub(action.data.userID, action.data.mesId, (mesId) => {
         sio.emit('action', {type: 'client/READ_CHAT', data: [mesId]})
     })
 };
 
 export const addMemberCon = (action, sio, io) => {
+    if (!action.data.userID) return;
     console.log('action func ' + JSON.stringify(action));
     addMemberPub(action.data.mesId, action.data.members, (data, receiverId) => {
         data = {...data, id: action.data.id};
@@ -45,6 +49,7 @@ export const addMemberCon = (action, sio, io) => {
 };
 
 export const removeMemberCon = (action, sio, io) => {
+    if (!action.data.userID) return;
     removeMemberPub(action.data.mesId, action.data.memberId, (data, receiverId) => {
         for (let i = 0; i < receiverId.length; ++i) {
             io.to(receiverId[i]).emit('action', {type: 'client/REMOVE_MEMBER', data: action.data})
@@ -53,6 +58,7 @@ export const removeMemberCon = (action, sio, io) => {
 };
 
 export const updateUICon = (action, sio, io) => {
+    if (!action.data.userID) return;
     updateUIPub(action.data.mesId, action.data.data, (data, receiverId) => {
         for (let i = 0; i < receiverId.length; ++i) {
             io.to(receiverId[i]).emit('action', {type: 'client/UPDATE_UI', data: action.data})
