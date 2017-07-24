b0bb8796a6a42357afce3f116ba348ffabbc1ba0
creatnx <daihoangpham@gmail.com>
2017-07-24 13:48:17 +0700

    ~~

---
+++ run-micro-3.sh
@@ -0,0 +1,9 @@
+pm2 kill &&
+
+mongo -u admin -p dbjfu9cyr82bt2cpqbtuiavcp49q7vp48cq09xpnbu --authenticationDatabase admin kajkai-store --eval "db.dropDatabase() && db.getCollection('categories').createIndex( { name: \"text\" } )" &&
+mongo -u admin -p dbjfu9cyr82bt2cpqbtuiavcp49q7vp48cq09xpnbu --authenticationDatabase admin kajkai-notification --eval "db.dropDatabase()" &&
+
+pm2 flush &&
+pm2 start ./command/micro-store/index-store.js &&
+pm2 start ./command/micro-search/index-search.js &&
+pm2 start ./command/micro-notification/index-notification.js
