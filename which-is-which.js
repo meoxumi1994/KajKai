af39b4c7a1ac6fdd69cfa0a8fd12f97dbfccf60e
creatnx <daihoangpham@gmail.com>
2017-07-25 17:44:43 +0700

    ~~

---
+++ which-is-which.js
@@ -0,0 +1,38 @@
+export const post = {
+    kajkai_1: {
+        service: {
+            comment: '54.254.219.177:8082',
+            socket: '54.254.219.177:8084'
+        }
+    },
+    kajkai_2: {
+        service: {
+            user: '54.255.192.30:8080',
+        }
+    },
+    kajkai_3: {
+        service:{
+            notification: '54.169.67.200:8086',
+            search: '54.169.67.200:8085',
+            store: '54.169.67.200:8081'
+        }
+    },
+    kajkai_4: {
+        ip: '',
+        service: {
+            chat: '13.229.57.22:8083',
+            event: '13.229.57.22'
+        }
+    }
+};
+
+export const rev = {
+    comment: 1,
+    socket: 1,
+    user: 2,
+    notification: 3,
+    search: 3,
+    store: 3,
+    chat: 4,
+    event: 4
+};
