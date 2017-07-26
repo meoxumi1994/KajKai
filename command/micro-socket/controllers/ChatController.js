1a75551482ee09a9be5b55886ef4651bb50bc43c
creatnx <daihoangpham@gmail.com>
2017-07-26 14:54:31 +0700

    ~~

--- command/micro-socket/controllers/ChatController.js
+++ command/micro-socket/controllers/ChatController.js
@@ -2,6 +2,7 @@ import { addNewMessagePub, getUnreadMessagePub, resetChatCountPub, readChatPub, 
 
 export const addNewMessageCon = (action, sio, io) => {
     console.log('action ' + JSON.stringify(action));
+    if (!action.data || !action.data.userID) return;
     addNewMessagePub(action.data, (mes, emitList) => {
         console.log('data ' + JSON.stringify({mes: mes, list: emitList}));
         for (let i = 0; i < emitList.length; ++i) {
@@ -20,7 +21,7 @@ export const getUnreadMessageCon = (userID, sio, io) => {
 };
 
 export const resetChatCountCon = (action, sio, io) => {
-    if (!action.data.userID) return;
+    if (!action.data || !action.data.userID) return;
     resetChatCountPub(action.data.userID, (res) => {
         sio.emit('action', {type: 'client/RESET_UNREAD_CHATS_QUANTITY', data: {
             quantity: 0,
@@ -29,14 +30,14 @@ export const resetChatCountCon = (action, sio, io) => {
 };
 
 export const readChatCon = (action, sio, io) => {
-    if (!action.data.userID) return;
+    if (!action.data || !action.data.userID) return;
     readChatPub(action.data.userID, action.data.mesId, (mesId) => {
         sio.emit('action', {type: 'client/READ_CHAT', data: [mesId]})
     })
 };
 
 export const addMemberCon = (action, sio, io) => {
-    if (!action.data.userID) return;
+    if (!action.data || !action.data.userID) return;
     console.log('action func ' + JSON.stringify(action));
     addMemberPub(action.data.mesId, action.data.members, (data, receiverId) => {
         data = {...data, id: action.data.id};
@@ -49,7 +50,7 @@ export const addMemberCon = (action, sio, io) => {
 };
 
 export const removeMemberCon = (action, sio, io) => {
-    if (!action.data.userID) return;
+    if (!action.data || !action.data.userID) return;
     removeMemberPub(action.data.mesId, action.data.memberId, (data, receiverId) => {
         for (let i = 0; i < receiverId.length; ++i) {
             io.to(receiverId[i]).emit('action', {type: 'client/REMOVE_MEMBER', data: action.data})
@@ -58,7 +59,7 @@ export const removeMemberCon = (action, sio, io) => {
 };
 
 export const updateUICon = (action, sio, io) => {
-    if (!action.data.userID) return;
+    if (!action.data || !action.data.userID) return;
     updateUIPub(action.data.mesId, action.data.data, (data, receiverId) => {
         for (let i = 0; i < receiverId.length; ++i) {
             io.to(receiverId[i]).emit('action', {type: 'client/UPDATE_UI', data: action.data})
