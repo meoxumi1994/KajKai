1b04750c09c72d4e4dee141a328910b5a124f4ba
charitynguyenvan <charitynguyenvan@gmail.com>
2017-07-27 14:14:39 +0700

    up code

--- query/view-user/services/NotificationService.js
+++ query/view-user/services/NotificationService.js
@@ -67,22 +67,18 @@ export const createLikeSellpostNotification = (sellpostId) => {
 }
 
 export const createLikeCommentNotification = (commentId) => {
-  console.log('createLikeCommentNotification');
   CommentLiker.findOne({ commentId }, (err, commentLiker) => {
     if (commentLiker) {
-      console.log('commentLiker: ', commentLiker);
       let { likers } = commentLiker
       const numberOfLike = likers.length
       const liker = likers[likers.length - 1]
       likers = likers.length <= 5 ? likers : likers.splice(likers.length - 5)
-      console.log('likers: ', likers);
       IDCommentSellpost.findOne({ commentId }, (err, mIDCommentSellpost) => {
         if (mIDCommentSellpost) {
           IDSellpostStore.findOne({ sellpostId: mIDCommentSellpost.sellpostId }, (err, mIDSellpostStore) => {
             if (mIDSellpostStore) {
               BasicStore.findOne({ id: mIDSellpostStore.storeId }, (err, basicStore) => {
                 if (basicStore) {
-                  console.log('mIDCommentSellpost.sellpostId: ', mIDCommentSellpost.sellpostId);
                   User.find({}, (err, users) => {
                     if (users) {
                       for (let i = 0; i < users.length; i++) {
