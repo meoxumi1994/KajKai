0f42469b97ec4e0f3ce916b2caf6e8ad90cdf400
creatnx <daihoangpham@gmail.com>
2017-07-27 16:56:05 +0700

    ~~

--- command/micro-socket/channels.js
+++ command/micro-socket/channels.js
@@ -2,5 +2,9 @@ export default {
     'STORE.Created': {
         controller: 'SocketSubController',
         method: 'notifyInterestSub'
+    },
+    'NOTIFICATION': {
+        controller: 'SocketSubController',
+        method: 'getNotification'
     }
 }
--- command/micro-socket/controllers/SocketSubController.js
+++ command/micro-socket/controllers/SocketSubController.js
@@ -1,8 +1,15 @@
 import { notifyInterestUser } from '../services/PublishStoreService'
+import { emitNotification } from '../services/SocketService'
 
 export const notifyInterestSub = (message) => {
     const { id, storeName, urlName, avatarUrl, coverUrl,
           secondCategoryId, secondCategoryName, longitude, latitude} = message.store;
     notifyInterestUser({ id, storeName, urlName, avatarUrl, coverUrl,
         secondCategoryId, secondCategoryName, longitude, latitude })
+};
+
+export const getNotification = (message) => {
+    const userId = message.notification.userId;
+    const notifyData = message.notification.data;
+    emitNotification([userId], 'global/NOTIFICATION', notifyData);
 };
