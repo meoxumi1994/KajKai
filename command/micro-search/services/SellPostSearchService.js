69df21f8fe3d36320fb4eec1eedac0fab7221cef
creatnx <daihoangpham@gmail.com>
2017-08-20 14:34:52 +0700

    ~~

--- command/micro-search/services/SellPostSearchService.js
+++ command/micro-search/services/SellPostSearchService.js
@@ -60,6 +60,8 @@ export const getDisplayResult = (hitsResult, offset, length) => {
         };
         res.push(sellPost);
     }
+    console.log(length + ' ' + res.length);
+    console.log(length === res.length);
     return {sellPosts: res, offset: (length === res.length) ? offset + length : -2};
 };
 
