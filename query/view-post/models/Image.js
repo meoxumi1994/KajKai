397925bb60ab2474792c80820d21f193defb3cdf
charitynguyenvan <charitynguyenvan@gmail.com>
2017-08-22 22:03:12 +0700

    up code

--- query/view-post/models/Image.js
+++ query/view-post/models/Image.js
@@ -1,9 +1,13 @@
 import mongoose from '../datasource'
+import _ from 'lodash'
+import { ImageType } from '../enum'
 
 const ImageSchema = new mongoose.Schema({
     url: {type: String},
     time: {type: Date},
-    postrowId: {type: String}
+    storeId: {type: String},
+    postrowId: {type: String},
+    type: {type: String, enum: _.values(ImageType)}
 })
 
 export default ImageSchema
