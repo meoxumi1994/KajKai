1b303e2fbe8def685a3791ca56f944125ae0d793
creatnx <daihoangpham@gmail.com>
2017-08-27 15:04:48 +0700

    ~~

--- command/micro-store/models/SubCategory.js
+++ command/micro-store/models/SubCategory.js
@@ -2,6 +2,7 @@ import mongoose from '../datasource'
 
 const SubCategorySchema = new mongoose.Schema({
     name: { type: String, required: true },
+    enName: {type: String}
 });
 
 export default SubCategorySchema;
