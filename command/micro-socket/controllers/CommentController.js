44f3939eeaed834dc7642b18dc4ea4e9e1dc78cc
creatnx <daihoangpham@gmail.com>
2017-08-29 18:06:00 +0700

    ~~

--- command/micro-socket/controllers/CommentController.js
+++ command/micro-socket/controllers/CommentController.js
@@ -72,8 +72,8 @@ export const addNewFirstLayerCommentCon = (action, sio, io) => {
             } else {
                 io.to(action.data.minorpostid).emit('action', {type: 'client/LEADERCOMMENT', data: fComment})
             }
-
-            let { phone, position, address, ...data } = fComment;
+            let data = fComment;
+            data = _objectWithoutProperties(x, ['phone', 'address', 'position']);
             getListFollower(action.data.sellpostid, (list) => {
                 const newId = fComment.user ? fComment.user : fComment.commenterid;
                 if (list.indexOf(newId) === -1) {
@@ -81,12 +81,7 @@ export const addNewFirstLayerCommentCon = (action, sio, io) => {
                 }
                 for (let i = 0; i < list.length; ++i) {
                     if (list[i].startsWith('002') || list[i] === action.data.userID) {
-                        io.to(list[i]).emit('action', {type: 'global/LEADERCOMMENT', data: {
-                            ...data,
-                            phone,
-                            position,
-                            address
-                        }});
+                        io.to(list[i]).emit('action', {type: 'global/LEADERCOMMENT', data: fComment});
                     } else {
                         io.to(list[i]).emit('action', {type: 'global/LEADERCOMMENT', data: data});
                     }
