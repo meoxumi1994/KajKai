f15b72a1eb891eddc39dba744270c0cc69db6711
longly <lyhoanglong94@gmail.com>
2017-08-29 12:17:30 +0700

    update

--- client/containers/user/index.js
+++ client/containers/user/index.js
@@ -2,6 +2,7 @@ import { connect } from 'react-redux'
 
 import User from '~/components/user'
 import { getUser, getInterest } from '~/actions/asyn/user'
+import { getChatList } from '~/actions/asyn/chat/restful'
 
 const mapStateToProps = (state, ownProps ) => {
     const { scrollTop, scrollLeft, height } = state.inst.app
@@ -19,7 +20,11 @@ const mapStateToProps = (state, ownProps ) => {
 const mapDispatchToProps = (dispatch, ownProps) => ({
     onGetUser: (id) => {
         dispatch(getUser(id))
-    }
+    },
+    getChatList: (id) => {
+        dispatch(getChatList(-1, 10))
+        dispatch({type: 'CURRENT_CHAT', subType: 'SET_USER_ID', data: {id: id}})
+    },
 })
 
 
